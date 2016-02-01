3d602803dfc96c28adfe831e80de93f1fa1659c8
Elmar Frank <elmar.frank@efrank.at>
2016-02-01 14:19:56 +0100

    events callback

--- Dragula.php
+++ Dragula.php
@@ -9,12 +9,47 @@ use yii\web\JsExpression;
 use yii\web\View;
 use yii\helpers\Url;
 
-
+/**
+ *
+ *	EXAMPLE
+ *	=======
+ *
+ * <?= \x1\dragula\Dragula::widget([
+ *     'containers' => ['#files'],
+ *     'options'    => [
+ *         'revertOnSpill' => true,
+ *     ],
+ *     'drop' => new \yii\web\JsExpression('my.dropped'),
+ * ]); ?>
+ * 
+ * 
+ * <script>
+ *     var my = {};
+ * 
+ *     my.dropped = function(el, container) {
+ *         var c      = $(container);
+ *         var items  = c.find('li[data-filename]');
+ *         var result = [];
+ *         $.each(c.find('li[data-filename]'), function(key, item) {
+ *             result.push($(item).data('filename'));
+ *         });
+ * 
+ *         console.log('result', result);
+ *         // your logic here, e.g. ajax post
+ *     }
+ * </script>
+ * 
+ */
 class Dragula extends \yii\base\Widget {
 
 	public $containers = null;
 	public $options    = null;
 
+	// events
+	public $drag = null;
+	public $drop = null;
+	public $over = null;
+	public $out  = null;
 
 	public function init() {
 		parent::init();
@@ -30,7 +65,19 @@ class Dragula extends \yii\base\Widget {
 
 	public function run() {
 		DragulaAsset::register($this->view);
-		$this->view->registerJs(sprintf('dragula(%s, %s);', Json::encode($this->containers), Json::encode($this->options)));
+
+		$events = [];
+		foreach (['drag', 'drop', 'over', 'out'] as $event) {
+			if ($this->$event !== null) {
+				if ($this->$event instanceof JsExpression) {
+					$events[] = new JsExpression(sprintf('.on("%s", %s)', $event, $this->$event));
+				} else {
+					$events[] = new JsExpression(sprintf('.on("%s", function(el, container) { %s })', $event, $this->$event));
+				}
+			}
+		}
+
+		$this->view->registerJs(sprintf('dragula(%s, %s)%s;', Json::encode($this->containers), Json::encode($this->options), implode('', $events)));
 		return null;
 	}
 
